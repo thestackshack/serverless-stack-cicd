5d62b34ce1d7b76fcbd1d446616581de6f231ec2
Findley, Randy <rgfindley@gmail.com>
2018-01-08 08:29:59 -0500

    Change Stage param.  Add better photo.

--- stacks/api/index.js
+++ stacks/api/index.js
@@ -4,7 +4,7 @@ exports.handler = function(event, context, callback) {
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            message: `Hello CIM`,
+            message: `Hello Serverless Stack CI/CD`,
             event: event
         })
     };
